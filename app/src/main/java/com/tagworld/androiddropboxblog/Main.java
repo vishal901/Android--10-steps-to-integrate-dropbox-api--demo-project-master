66506153fdf61da07d15025d28f461accefc67aa
vishal <vishal.patel77851@gmail.com>
2016-06-29 17:05:22 +0530

    direct upload csv

--- app/src/main/java/com/tagworld/androiddropboxblog/Main.java
+++ app/src/main/java/com/tagworld/androiddropboxblog/Main.java
@@ -1,7 +1,15 @@
 package com.tagworld.androiddropboxblog;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,7 +17,9 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -29,6 +39,15 @@ public class Main extends Activity implements OnClickListener {
 	private boolean mLoggedIn, onResume;
 	private DropboxAPI<AndroidAuthSession> mApi;
 
+	private static boolean writeCSVToConsole = true;
+	private static boolean writeCSVToFile = true;
+
+	private static boolean sortTheList = true;
+	private static final String IMAGE_DIRECTORY_NAME = "MyData";
+	public static String timeStamp;
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	File mediaFile;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,6 +62,10 @@ public class Main extends Activity implements OnClickListener {
 		btnUpload = (Button) findViewById(R.id.btnUploadPhoto);
 		btnUpload.setOnClickListener(this);
 		btnDownload.setOnClickListener(this);
+
+		List<String> sampleList = createSampleList();
+
+		convertAndPrint(sampleList, writeCSVToConsole, writeCSVToFile, sortTheList);
 	}
 
 	private AndroidAuthSession buildSession() {
@@ -87,10 +110,26 @@ public class Main extends Activity implements OnClickListener {
 			if (mLoggedIn) {
 				logOut();
 			}
-			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-			f = new File(Utils.getPath(),new Date().getTime()+".jpg");
-			intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
-			startActivityForResult(intent, TAKE_PHOTO);
+
+			if (Utils.isOnline(Main.this)) {
+				mApi.getSession().startAuthentication(Main.this);
+				onResume = true;
+			} else {
+				Utils.showNetworkAlert(Main.this);
+			}
+
+			mLoggedIn = false;
+			if (false) {
+				UploadFile upload = new UploadFile(Main.this, mApi, DIR, mediaFile);
+				upload.execute();
+				//onResume = false;
+
+			}
+
+//			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//			f = new File(Utils.getPath(),new Date().getTime()+".jpg");
+//			intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+//			startActivityForResult(intent, TAKE_PHOTO);
 		}
 	}
 
@@ -134,7 +173,7 @@ public class Main extends Activity implements OnClickListener {
 	public void setLoggedIn(boolean loggedIn) {
 		mLoggedIn = loggedIn;
 		if (loggedIn) {
-			UploadFile upload = new UploadFile(Main.this, mApi, DIR, f);
+			UploadFile upload = new UploadFile(Main.this, mApi, DIR, mediaFile);
 			upload.execute();
 			onResume = false;
 
@@ -173,5 +212,81 @@ public class Main extends Activity implements OnClickListener {
 			}
 		}
 		super.onResume();
+	}
+
+	private void convertAndPrint(List<String> sampleList,
+								 boolean writeToConsole, boolean writeToFile, boolean sortTheList) {
+
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				IMAGE_DIRECTORY_NAME);
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				// MyApplication.getInstance().showLog("TAG", "Oops! Failed create " + IMAGE_DIRECTORY_NAME + " directory");
+
+			}
+		}
+
+		timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+				Locale.getDefault()).format(new Date());
+
+
+		mediaFile = new File(mediaStorageDir.getPath() + File.separator
+				+ "file_" + timeStamp + ".csv");
+
+		String commaSeparatedValues = "";
+
+		/** If the list is not null and the list size is not zero, do the processing**/
+		if (sampleList != null) {
+			/** Sort the list if sortTheList was passed as true**/
+			if (sortTheList) {
+				Collections.sort(sampleList);
+			}
+			/**Iterate through the list and append comma after each values**/
+			Iterator<String> iter = sampleList.iterator();
+			while (iter.hasNext()) {
+				commaSeparatedValues += iter.next() + ",";
+			}
+			/**Remove the last comma**/
+			if (commaSeparatedValues.endsWith(",")) {
+				commaSeparatedValues = commaSeparatedValues.substring(0,
+						commaSeparatedValues.lastIndexOf(","));
+			}
+		}
+		/** If writeToConsole flag was passed as true, output to console**/
+		if (writeToConsole) {
+			System.out.println(commaSeparatedValues);
+		}
+		/** If writeToFile flag was passed as true, output to File**/
+		if (writeToFile) {
+
+			Log.e("calling","method");
+			try {
+				FileWriter fstream = new FileWriter(mediaFile, false);
+				BufferedWriter out = new BufferedWriter(fstream);
+				out.write(commaSeparatedValues);
+				out.close();
+				System.out.println("*** Also wrote this information to file: " + mediaFile);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+
+			Log.e("calling","method");
+		}
+
+	}
+
+
+	private List<String> createSampleList() {
+		List<String> sampleList = new ArrayList<String>();
+		sampleList.add("Nebraska");
+		sampleList.add("Iowa");
+		sampleList.add("Illinois");
+		sampleList.add("Idaho");
+		return sampleList;
 	}
 }
